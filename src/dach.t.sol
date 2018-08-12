071ca16fa3f0c7531d70532fc59c67e170a5ab80
Martin Lundfall <martin.lundfall@protonmail.com>
2018-08-12 13:30:34 +0200

    New memes

--- src/dach.t.sol
+++ src/dach.t.sol
@@ -2,32 +2,32 @@ pragma solidity ^0.4.23;
 
 import "ds-test/test.sol";
 
-import "./Relay.sol";
+import "./dach.sol";
 
 contract Lad {
 }
 
-contract RelayTest is DSTest {
-    Relay relay;
+contract DachTest is DSTest {
+    Dach dach;
     Mover mover;
     Lad ali;
     Lad bob;
     address cal = 0x29c76e6ad8f28bb1004902578fb108c507be341b;
     address del = 0xdd2d5d3f7f1b35b7a0601d6a00dbb7d44af58479;
-    uint wad = 2;
+    uint amount = 2;
     uint fee = 1;
     uint nonce = 0;
     //output from generateSigs.js; a signed, typed message from cal of a cheque
     //specifying a 2 dai transfer to del with a 1 dai fee to msg.sender
-    string sig = '0x657e67032cfa3b1efd231d5b046e07bb2ecf1547b9d32551c3793fe156516ad00a811c31e50e64adea6a7209836fc22d7746a67d3953a342453857daf26704841b';
+    string sig = '0x1c539435b3cc80a7354add9b1198092632f900934b336f8d0a75e40cc1ddf7da0b8918b6953d6a88954a434303bf0a41accd811a1a5ca70ca6444a6c8c8ec0151c';
     //the string above is not used anywhere, we decompose it into the following params:
-    uint8 v = 27;
-    bytes32 r = 0x657e67032cfa3b1efd231d5b046e07bb2ecf1547b9d32551c3793fe156516ad0;
-    bytes32 s = 0x0a811c31e50e64adea6a7209836fc22d7746a67d3953a342453857daf2670484;
+    uint8 v = 28;
+    bytes32 r = 0x1c539435b3cc80a7354add9b1198092632f900934b336f8d0a75e40cc1ddf7da;
+    bytes32 s = 0x0b8918b6953d6a88954a434303bf0a41accd811a1a5ca70ca6444a6c8c8ec015;
 
     function setUp() public {
-      relay = new Relay();
-      mover = Mover(relay.mover());
+      dach = new Dach();
+      mover = Mover(dach.mover());
       ali = new Lad();
       bob = new Lad();
       mover.mint(ali, 100);
@@ -54,22 +54,18 @@ contract RelayTest is DSTest {
     }
 
     function test_domain_sep() public {
-      assertEq(relay.DOMAIN_SEPARATOR(), 0xcefc3efd3e12749cf6849e637e99c71315209ccc419b8d6a6c967a71e7edd86b);
+      assertEq(dach.DOMAIN_SEPARATOR(), 0xb2240f1d5346f6d79d915a641f2e3ff57024d09b05d1ee76fda676119b4de90b);
     }
 
     function test_cheque_typehash() public {
-      assertEq(relay.CHEQUE_TYPEHASH(), 0x7eb02bee71261bc514e2fa911172c93f74df70e1e57befd4a626f0ab26784c42);
+      assertEq(dach.CHEQUE_TYPEHASH(), 0x3f2386d9e00bfe3dbbdeb444816f2d701398001e2c2b9051190e2198f4f46caa);
     }
 
-    function test_cheque_hash() public {
-      assertEq(relay.hash(cal, del, 2, 1, 0), 0x2383830092099c0f994fe5f6771c373c12bcfcafc664b5ab235b372265068b17);
-    }
-
-    function test_relay() public {
+    function test_clear() public {
       assertEq(mover.balanceOf(cal),80);
       assertEq(mover.balanceOf(del),0);
       assertEq(mover.balanceOf(this),0);
-      relay.relay(cal, del, 2, 1, 0, v, r, s);
+      dach.clear(cal, del, 2, 1, 0, v, r, s);
       assertEq(mover.balanceOf(cal),77);
       assertEq(mover.balanceOf(del),2);
       assertEq(mover.balanceOf(this),1);
@@ -77,7 +73,7 @@ contract RelayTest is DSTest {
 
     function test_replay_protection() public {
       //Resubmitting the same cheque results in a throw
-      relay.relay(cal, del, 2, 1, 0, v, r, s);
+      dach.clear(cal, del, 2, 1, 0, v, r, s);
       assertEq(mover.balanceOf(cal),77);
       assertEq(mover.balanceOf(del),2);
       assertEq(mover.balanceOf(this),1);
